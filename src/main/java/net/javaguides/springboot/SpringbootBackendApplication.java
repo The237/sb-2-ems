f0d802ae1fe422e54d02c2e3222a5126a4b4d12a
The237 <molingojorge@gmail.com>
2023-04-19 13:05:32 +0100

    Managa CORS issues

--- src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
+++ src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -2,6 +2,9 @@ package net.javaguides.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootBackendApplication {
@@ -10,4 +13,13 @@ public class SpringbootBackendApplication {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/v1/employees").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
 }
