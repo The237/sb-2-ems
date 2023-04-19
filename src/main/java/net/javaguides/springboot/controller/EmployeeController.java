27ac827c59c9253a13b7d5b71919fab6729df4e4
The237 <69681767+The237@users.noreply.github.com>
2023-04-19 19:01:00 +0100

    get employee by Id functionnality

--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -3,16 +3,16 @@ package net.javaguides.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
 import net.javaguides.springboot.model.Employee;
-import net.javaguides.springboot.exception.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import net.javaguides.springboot.exception.ResourceNotFoundException;
 
 import net.javaguides.springboot.repository.EmployeeRepository;
 
