6aef0e28ff93a6c719d0e138cf7bf7a4eb2a1bf7
The237 <69681767+The237@users.noreply.github.com>
2023-04-20 12:27:55 +0100

    add  deleteEmployee REST API endpoint

--- src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
+++ src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -2,9 +2,9 @@ package net.javaguides.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootBackendApplication {
@@ -20,9 +20,7 @@ public class SpringbootBackendApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry
-						.addMapping("/api/v1/employees/**")
-						.allowedOrigins("http://localhost:4200")
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+						.addMapping("/api/v1/employees/**");
 			}
 		};
 	}
--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -1,18 +1,15 @@
 package net.javaguides.springboot.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import net.javaguides.springboot.model.Employee;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import net.javaguides.springboot.exception.ResourceNotFoundException;
 
 import net.javaguides.springboot.repository.EmployeeRepository;
@@ -44,6 +41,7 @@ public class EmployeeController {
         return ResponseEntity.ok(employee);
     }
 
+    // update employee REST API
     @PutMapping("/employees/{id}")
     public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
         Employee employee = employeeRepository.findById(id)
@@ -53,5 +51,16 @@ public class EmployeeController {
         employee.setEmailId(employeeDetails.getEmailId());
         Employee updatedEmployee = employeeRepository.save(employee);
         return ResponseEntity.ok(updatedEmployee);
+    }
+
+    // delete employee REST API
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+        employeeRepository.delete(employee);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
     }
 }
