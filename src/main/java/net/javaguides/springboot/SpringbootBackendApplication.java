9e4fea75a56263a7064cb6fcfe2b3f142cb8ff9b
The237 <69681767+The237@users.noreply.github.com>
2023-04-20 12:50:50 +0100

    Clear the error generated by code control tool

--- src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
+++ src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -3,10 +3,6 @@ package net.javaguides.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-<<<<<<< HEAD
-import org.springframework.context.annotation.Configuration;
-=======
->>>>>>> 07-deleteEmployee
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
